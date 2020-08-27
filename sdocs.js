dcddbb55c015688a902cc202456292f4a1af07b5
nhevia <nicolashevia@hotmail.com>
2020-08-27 13:08:43 +0200

    feat: init

---
+++ sdocs.js
@@ -0,0 +1,43 @@
+const fsp = require('fs').promises;
+const fs = require('fs');
+const glob = require("glob");
+
+const filepath = './docs.md'
+
+const options = {
+  ignore: '**/node_modules/**'
+}
+
+const files = glob.sync("**/**.+(js|jsx|ts|tsx)", options)
+
+const readFile = file => {
+  return new Promise((resolve) => {
+    fs.readFile(file, {encoding: 'utf-8'}, function (err, data) {
+      if (err) throw err;
+      
+      const start = data.indexOf('/*sdoc')
+      if (start > -1 && start < 10 ) {
+        const ends = data.indexOf('*/')
+        resolve({filename: file, data: data.slice(start + 7, ends).trim()})
+      } else {
+        resolve('')
+      }     
+    });
+  })
+}
+
+const readFiles = async () => {
+  return Promise.all(files.map(item => readFile(item)))
+}
+
+const parseData = async () => {
+  const read = await readFiles()
+  const filtered = read.filter(el => el !== '')
+  const textToWrite = filtered.map(el => `>${el.filename}\n\n  ${el.data}`)
+  return textToWrite.join('\n')
+}
+
+
+(async () => {
+  await fsp.writeFile(filepath, await parseData(), 'utf8');
+})();
